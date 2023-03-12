4b5758f21d9344256b90ebc0313ef922342e69df
samuel cho <samonthepiano@gmail.com>
2023-03-12 10:47:00 +0100

    implement find normal order of pitch class set

--- src/components/AnalysisDisplay.js
+++ src/components/AnalysisDisplay.js
@@ -1,5 +1,46 @@
 import React from "react";
 
+function cycleAdd12(arr){
+  return arr.slice(1).concat(arr[0]+12)
+}
+
+function circularPermutationsAdd12(arr){
+  let currPermut = arr.slice()
+  let permutations = [currPermut]
+  for(let i = 0; i < arr.length-1;i++){
+    currPermut = cycleAdd12(currPermut)
+    permutations.push(currPermut)
+  }
+  return permutations
+}
+
+function getDistanceArray(arr){
+  let indices = [];
+  for(let i = 1; i < arr.length; i++){
+    indices.push((i+arr.length-3)%(arr.length-1)+1)
+  }
+  let distanceArray = indices.map(i=>Math.abs(arr[0]-arr[i]))
+  return distanceArray
+}
+
+function findNormalOrder(arr){
+  let permutations = circularPermutationsAdd12(arr);
+  permutations = permutations.map(p=>{
+    return {permutation: p,
+            distanceArray: getDistanceArray(p)}})
+  for(let i = 0; i < arr.length-1; i++){
+    let distances = permutations.map(p=>p.distanceArray[i])
+    let minDist = Math.min(...distances)
+    permutations = permutations.filter(p=>p.distanceArray[i]===minDist)
+    if(permutations.length === 1){
+      return permutations[0].permutation
+    } else {
+      return permutations[0].permutation
+    }
+  }
+
+}
+
 function arrayToString(arr) {
   let result = "";
   arr.forEach((item) => {
@@ -20,6 +61,7 @@ class Analysis extends React.Component {
       <div className="analysis">
         <div>Ordered: {arrayToString(this.props.pcset)}</div>
         <div>Unordered: {arrayToString(sortAscending(this.props.pcset))}</div>
+        <div>Normal Order:{arrayToString(findNormalOrder(this.props.pcset))}</div>
       </div>
     );
   }
