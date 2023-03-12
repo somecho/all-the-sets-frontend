2dac83ce29c8f6990c2667fc95a8b023bea3b1a9
samuel cho <samonthepiano@gmail.com>
2023-03-12 11:13:30 +0100

    fix finding normal to make sure input is in ascending order

--- src/components/AnalysisDisplay.js
+++ src/components/AnalysisDisplay.js
@@ -1,44 +1,47 @@
 import React from "react";
 
-function cycleAdd12(arr){
-  return arr.slice(1).concat(arr[0]+12)
+function cycleAdd12(arr) {
+  return arr.slice(1).concat(arr[0] + 12);
 }
 
-function circularPermutationsAdd12(arr){
-  let currPermut = arr.slice()
-  let permutations = [currPermut]
-  for(let i = 0; i < arr.length-1;i++){
-    currPermut = cycleAdd12(currPermut)
-    permutations.push(currPermut)
+function circularPermutationsAdd12(arr) {
+  let currPermut = arr.slice();
+  let permutations = [currPermut];
+  for (let i = 0; i < arr.length - 1; i++) {
+    currPermut = cycleAdd12(currPermut);
+    permutations.push(currPermut);
   }
-  return permutations
+  return permutations;
 }
 
-function getDistanceArray(arr){
+function getDistanceArray(arr) {
   let indices = [];
-  for(let i = 1; i < arr.length; i++){
-    indices.push((i+arr.length-3)%(arr.length-1)+1)
+  for (let i = 1; i < arr.length; i++) {
+    indices.push(((i + arr.length - 3) % (arr.length - 1)) + 1);
   }
-  let distanceArray = indices.map(i=>Math.abs(arr[0]-arr[i]))
-  return distanceArray
+  let distanceArray = indices.map((i) => Math.abs(arr[0] - arr[i]));
+  return distanceArray;
 }
 
-function findNormalOrder(arr){
+function findNormalOrder(arr) {
   let permutations = circularPermutationsAdd12(arr);
-  permutations = permutations.map(p=>{
-    return {permutation: p,
-            distanceArray: getDistanceArray(p)}})
-  for(let i = 0; i < arr.length-1; i++){
-    let distances = permutations.map(p=>p.distanceArray[i])
-    let minDist = Math.min(...distances)
-    permutations = permutations.filter(p=>p.distanceArray[i]===minDist)
-    if(permutations.length === 1){
-      return permutations[0].permutation
-    } else {
-      return permutations[0].permutation
+  permutations = permutations.map((p) => {
+    return { permutation: p, distanceArray: getDistanceArray(p) };
+  });
+  for (let i = 0; i < arr.length - 1; i++) {
+    let distances = permutations.map((p) => p.distanceArray[i]);
+    let minDist = Math.min(...distances);
+    permutations = permutations.filter((p) => p.distanceArray[i] === minDist);
+    console.log(permutations);
+    if (permutations.length === 1) {
+      return permutations[0].permutation.map((i) => i % 12);
     }
   }
+  return permutations[0].permutation.map((i) => i % 12);
+}
 
+function makePrime(normalOrder) {
+  return normalOrder.map((i) => (i - normalOrder[0]) % 12);
 }
 
 function arrayToString(arr) {
@@ -57,11 +60,16 @@ function sortAscending(arr) {
 
 class Analysis extends React.Component {
   render() {
+    let pcset = this.props.pcset;
+    let ascending = sortAscending(pcset);
+    let normal = findNormalOrder(ascending);
+    let prime = makePrime(normal);
     return (
       <div className="analysis">
-        <div>Ordered: {arrayToString(this.props.pcset)}</div>
-        <div>Unordered: {arrayToString(sortAscending(this.props.pcset))}</div>
-        <div>Normal Order:{arrayToString(findNormalOrder(this.props.pcset))}</div>
+        <div>Ordered: {arrayToString(pcset)}</div>
+        <div>Unordered: {arrayToString(ascending)}</div>
+        <div>Normal Order:{arrayToString(normal)}</div>
+        <div> Prime form: {arrayToString(prime)} </div>
       </div>
     );
   }
