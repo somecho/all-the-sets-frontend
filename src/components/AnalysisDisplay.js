23720aa06540fbf80a1446e70ff7e613bd918ae2
samuel cho <samonthepiano@gmail.com>
2023-03-20 20:11:24 +0100

    move calculations locally

--- src/components/AnalysisDisplay.js
+++ src/components/AnalysisDisplay.js
@@ -1,117 +1,13 @@
 import React from "react";
 import "./AnalysisDisplay.css";
-import { Table, Header } from "semantic-ui-react";
-
-function mod(a, b) {
-  return ((a % b) + b) % b;
-}
-function calculateIntervalVector(pcs) {
-  let size = pcs.length;
-  let LUT = [0, 0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0];
-  let intervalVector = [0, 0, 0, 0, 0, 0];
-  for (let i = 0; i < size; i++) {
-    for (let j = i + 1; j < size; j++) {
-      let diff = Math.abs(pcs[i] - pcs[j]) % 12;
-      let id = LUT[diff];
-      intervalVector[id]++;
-    }
-  }
-  return intervalVector;
-}
-
-function cycleAdd12(arr) {
-  return arr.slice(1).concat(arr[0] + 12);
-}
-
-function circularPermutationsAdd12(arr) {
-  let currPermut = arr.slice();
-  let permutations = [currPermut];
-  for (let i = 0; i < arr.length - 1; i++) {
-    currPermut = cycleAdd12(currPermut);
-    permutations.push(currPermut);
-  }
-  return permutations;
-}
-
-function getDistanceArray(arr) {
-  let indices = [];
-  for (let i = 1; i < arr.length; i++) {
-    indices.push(((i + arr.length - 3) % (arr.length - 1)) + 1);
-  }
-  let distanceArray = indices.map((i) => Math.abs(arr[0] - arr[i]));
-  return distanceArray;
-}
-
-function findNormalOrder(arr) {
-  let permutations = circularPermutationsAdd12(arr);
-  permutations = permutations.map((p) => {
-    return { permutation: p, distanceArray: getDistanceArray(p) };
-  });
-  for (let i = 0; i < arr.length - 1; i++) {
-    let distances = permutations.map((p) => p.distanceArray[i]);
-    let minDist = Math.min(...distances);
-    permutations = permutations.filter((p) => p.distanceArray[i] === minDist);
-    if (permutations.length === 1) {
-      return permutations[0].permutation.map((i) => i % 12);
-    }
-  }
-  return permutations[0].permutation.map((i) => i % 12);
-}
-
-function arrayToCommaString(arr) {
-  let result = "";
-  for (let i = 0; i < arr.length; i++) {
-    result += arr[i];
-    if (i !== arr.length - 1) {
-      result += ",";
-    }
-  }
-  return result;
-}
-function invertSet(pcset) {
-  return pcset.map((pc) => mod(12 - pc, 12));
-}
-async function makePrime(normalOrder) {
-  //First check the current normal order
-  let primeOrder = normalOrder.map((i) => mod(i - normalOrder[0], 12));
-  let res = await fetch(
-    `https://all-the-sets.onrender.com/pcs/${arrayToCommaString(primeOrder)}`
-  );
-  let data = await res.json();
-  if (data.pcs) {
-    return { primeOrder: data.pcs, name: data.name, zrelated: data.zrelated };
-  }
-  //If it doesn't exist, invert, sort ascending and get the new normal order
-  //transposed to 0
-  let inversion = invertSet(primeOrder);
-  let ascendingInversion = sortAscending(inversion);
-  let normal = findNormalOrder(ascendingInversion);
-  normal = normal.map((i) => mod(i - normal[0], 12));
-  res = await fetch(
-    `https://all-the-sets.onrender.com/pcs/${arrayToCommaString(normal)}`
-  );
-  data = await res.json();
-  if (data.pcs) {
-    return { primeOrder: data.pcs, name: data.name, zrelated: data.zrelated };
-  }
-  return { primeOrder: [], name: "No prime found" };
-}
-
-// fetch("https://all-the-sets.onrender.com/pcs/0,2,3,6,9")
-//   .then((response)=>response.json())
-//   .then((data)=>console.log(data))
+import { Table, Header} from "semantic-ui-react";
+import {findPrime, calculateIntervalVector, findNormalOrder, sortAscending} from "../set-operations"
 
 function arrayToString(arr) {
   let result = "";
   arr.forEach((item) => {
     result += `${item} `;
   });
-  return result;
-}
-
-function sortAscending(arr) {
-  let result = arr.slice();
-  result.sort((a, b) => a - b);
   return result;
 }
 
@@ -151,7 +47,9 @@ class Analysis extends React.Component {
           </Table.Row>
           <Table.Row>
             <Table.Cell>Set Name</Table.Cell>
-            <Table.Cell>{this.props.name}</Table.Cell>
+            <Table.Cell>
+              {this.props.name}
+            </Table.Cell>
           </Table.Row>
           <Table.Row>
             <Table.Cell>Interval Vector</Table.Cell>
@@ -183,8 +81,6 @@ class AnalysisDisplay extends React.Component {
       ascending,
       normal,
       prime: [],
-      calculating: false,
-      requestQueue: [],
       zrelated: {},
     };
   }
@@ -196,29 +92,14 @@ class AnalysisDisplay extends React.Component {
     ) {
       let ascending = sortAscending(this.props.pcset);
       let normal = findNormalOrder(ascending);
-      let requestQueue = this.state.requestQueue.slice();
-      if (requestQueue.length > 3) {
-        requestQueue = requestQueue.slice(-3);
-      }
-      requestQueue.push(makePrime(normal));
-
+      let prime = findPrime(normal)
       this.setState({
         ascending,
         normal,
-        prime: "Calculating...",
+        prime: prime.primeOrder,
         calculating: true,
-        requestQueue,
-      });
-
-      //using Queue ensures latest request is latest result
-      Promise.all(requestQueue).then((values) => {
-        let latest = values[values.length - 1];
-        this.setState({
-          prime: latest.primeOrder,
-          primeName: latest.name,
-          calculating: false,
-          zrelated: latest.zrelated,
-        });
+        primeName: prime.name,
+        zrelated: prime.zrelated
       });
     }
   }
